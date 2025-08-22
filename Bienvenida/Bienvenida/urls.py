19f36377b219a22dc866aa69ffa23f9aee4042f6
benjajTempini <benjamin.rojas71@inacapmail.cl>
2025-08-22 16:17:23 -0400

    feat: Hola mundo desde django
    actividad de la clase 02 para el ramo de backend donde fabricamos nuestro primer hola mundo desde django :DDD

--- Bienvenida/Bienvenida/urls.py
+++ Bienvenida/Bienvenida/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from  . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('inicio/', views.inicio),
+    path('mostrar_bienvenida/', views.mostrar_bienvenida),
+    path('/', views.home),
 ]
---
+++ Bienvenida/Bienvenida/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+
+def inicio(request):
+    return HttpResponse("Hola mundo desde django")
+
+def mostrar_bienvenida(request):
+    tu_nombre = "Benjamin Rojas"
+    return HttpResponse(f"Bienvenidos a mi app Django, {tu_nombre}!")
+
+def home(request):
+    return HttpResponse("Quetu ace aca?? vete a jugar ROCKET LEAGUE")
