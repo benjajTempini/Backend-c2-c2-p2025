fe81724d2c1a29b37dd084a4da945c9ad6964096
BenjajTempini <benjamin.rojas71@inacapmail.cl>
2025-09-01 16:50:47 -0400

    feat: añadimos crud y todo jaskjas
    Introduces a new 'inventario' Django app with models, forms, views, templates, and URL routing for managing 'Producto' objects (CRUD operations). Updates project settings and main URL configuration to include the new app. Also adds explanatory HTML elements to productos/lista.html.

--- Bienvenida/Bienvenida/urls.py
+++ Bienvenida/Bienvenida/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from  . import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('inicio/', views.inicio),
     path('mostrar_bienvenida/', views.mostrar_bienvenida),
     path('', views.home),
-    path('productos/', views.lista_producto, name= "Productos"),
+
+    path('', include('inventario.urls')),
 ]
